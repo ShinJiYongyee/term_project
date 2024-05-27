140b1ebf7f464dfdba64b21d2fb1c3e9a28f149e
Shin Jiyong <shinjiyong0904@gmail.com>
2024-05-27 19:47:50 +0900

    insert.php 틀 완성

---
+++ insert.php
@@ -0,0 +1,20 @@
+<HTML>
+<HEAD>
+    <META http-equiv="content-type" content="text/html; charset=utf-8">
+</HEAD>
+<BODY>
+
+<H1>새 프로젝트 정보 입력</H1>
+<FORM METHOD="post" ACTION="insert_result.php">
+    프로젝트명 : <INPUT TYPE="text" NAME="projectname"> <BR>
+    정보 : <INPUT TYPE="text" NAME="intel"> <BR>
+    단계 : <INPUT TYPE="text" NAME="phase"> <BR>
+    직원 : <INPUT TYPE="text" NAME="employee"> <BR>
+    직원 : <INPUT TYPE="text" NAME="employee"> <BR>
+    날짜 : <INPUT TYPE="text" NAME="date"> <BR>
+    <BR><BR>
+    <INPUT TYPE="submit" VALUE="정보 입력">
+</FORM>
+
+</BODY>
+</HTML>
