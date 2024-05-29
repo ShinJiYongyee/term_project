002df4153b088b7d3d7c8d03757fb6717e7313ec
Shin Jiyong <shinjiyong0904@gmail.com>
2024-05-29 21:40:50 +0900

    projectdb 변경

--- projectDB.sql
+++ projectDB.sql
@@ -18,5 +18,3 @@ CREATE TABLE managerTBL (
     department varchar(12) not null,
     email VARCHAR(100)
 );
-insert into managerTBL values("sjy","1234","채찍질하는 감독관","백엔드 관리","back@gmail.com");
-insert into projectTBL values ("게르마늄팔찌","팔찌 제작","예산확보","김삼식","2024-07-07");
--- projectDB_test.sql
+++ projectDB_test.sql
@@ -1,5 +1,14 @@
+use projectDB;
+
 select * from managerTBL;
 select * from projectTBL;
-
+/*
 insert into managerTBL values("sjy","1234","채찍질하는 감독관","백엔드 관리","back@gmail.com");
+*/
 insert into projectTBL values ("게르마늄팔찌","팔찌 제작","예산확보","김삼식","2024-07-07");
+insert into projectTBL values ("게르마늄발찌","발찌 제작","공정 가동","김삼식","2024-08-07");
+insert into projectTBL values ("게르마늄팔찌2","팔찌 제작","예산확보","김삼식","2024-07-07");
+insert into projectTBL values ("게르마늄팔찌3","팔찌 제작","예산확보","김삼식","2024-07-07");
+insert into projectTBL values ("게르마늄팔찌4","팔찌 제작","예산확보","김삼식","2024-07-07");
+insert into projectTBL values ("게르마늄팔찌5","팔찌 제작","예산확보","김삼식","2024-07-07");
+
